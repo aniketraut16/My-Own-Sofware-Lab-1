95f826c2bae1c11002d07ab0e90da9b7b6ad3211
Aniket Raut <138971524+aniketraut16@users.noreply.github.com>
2024-02-06 11:29:27 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,2 +1,27 @@
-name = input("Enter your name:")
-print(f'Name:{name}')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello_world2():
+    return 'Hello World!'
+
+@app.route("/name")
+def name():
+    return 'Ayush'
+
+@app.route("/name/<myname>")
+def myname(myname):
+    return f'Hello, I am {myname}!'
+
+@app.route("/age/<value>")
+def hello_world4(value):
+    return f'My age is {value}!'
+
+@app.route("/info/<name>/<age>")
+def info(name, age):
+    return f'I am {name}. My age is {age}'
